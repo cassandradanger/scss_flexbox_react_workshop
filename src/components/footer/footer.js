785737bccdd6f49378673108dd4faf66e5714469
Casie Siekman <casiesiekman@Casies-MBP-2.fios-router.home>
2021-09-27 17:01:02 -0400

    footer styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,8 +5,10 @@ class Footer extends Component {
   render() {
     return (
         <div className="footer">
-          <p>these are some of casie's favorite recipes</p>
-          <p>mmm mmm, that's good food! © ℗®™</p>
+          <span>
+            <p>these are some of casie's favorite recipes</p>
+            <p>mmm mmm, that's good food! © ℗®™</p>
+          </span>
           <img src="https://5iyg119fkst3r2lyyhftyvwd-wpengine.netdna-ssl.com/wp-content/uploads/2021/08/casie.jpg" alt="casie with pink hair"/>
         </div>
   )}
