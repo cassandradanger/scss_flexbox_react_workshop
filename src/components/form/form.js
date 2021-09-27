998a9c80dd7defee1dc1ddf4f0fcb0e8e1faac03
Casie Siekman <casiesiekman@Casies-MBP-2.fios-router.home>
2021-09-27 15:11:31 -0400

    variables, mixins and starting to style the form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './form.css';
+import './form.scss';
 
 class Form extends Component {
   state = {
@@ -33,21 +33,25 @@ class Form extends Component {
   render() {
     return (
         <div className="form">
-          <div>
+          <div className="input-form">
             <label htmlFor="name">name:</label>
             <input type="text" id="name" onChange={(e) => {this.onChange(e, "name")}}/>
             <label htmlFor="imageUrl">image url:</label>
             <input type="text" id="imageUrl" onChange={(e) => {this.onChange(e, "imageUrl")}}/>
             <label htmlFor="recipeUrl">recipe url:</label>
             <input type="text" id="recipeUrl" onChange={(e) => {this.onChange(e, "recipeUrl")}}/>
-            <button onClick={this.onSubmit}>submittt</button>
+            <button onClick={this.onSubmit}>SUBMIT</button>
           </div>
           {/* { JSON.stringify(this.state.displayObject) } */}
           <div className="display-new-image">
-            <p>{this.state.displayObject.name}</p>
-            <a href={this.state.displayObject.recipeUrl}>
-              <img alt='most recent entry' src={this.state.displayObject.imageUrl}/>
-            </a>
+            {this.state.displayObject.name &&
+              <span>
+                <p>{this.state.displayObject.name}</p>
+                <a href={this.state.displayObject.recipeUrl}>
+                  <img alt='most recent entry' src={this.state.displayObject.imageUrl}/>
+                </a>
+              </span>
+            }
           </div>
         </div>
   )}
